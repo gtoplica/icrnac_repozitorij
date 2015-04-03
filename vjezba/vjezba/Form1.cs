dfd967bc463be8f01386c560712dd10eb893204c
icrnac <ivona.black4@gmail.com>
2015-04-03 12:46:18 +0200

    veci broj
    program za odredivanje veceg od 2 random generirana broja

---
+++ vjezba/vjezba/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vjezba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<int> listabrojeva = new List<int>();
+            Random random_br = new Random();
+
+            for (int i = 0; i < 2; i++)
+            {
+                int broj = random_br.Next(1, 100);
+                listabrojeva.Add(broj);
+
+        
+            }
+
+            textBox1.Text = listabrojeva[0].ToString();
+            textBox2.Text = listabrojeva[1].ToString();
+
+            int najveci = listabrojeva.Max();
+
+            textBox3.Text = najveci.ToString();
+        }
+
+
+    }
+}
